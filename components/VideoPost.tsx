5321d4662af4e9e198b4f83407c224419c049f53
yvetter438 <yannick.vetter@outlook.com>
2025-09-07 20:02:18 -0700

    changed restaurant popup background to transluscent

--- components/VideoPost.tsx
+++ components/VideoPost.tsx
@@ -654,7 +654,7 @@ const styles = StyleSheet.create({
   },
   modalOverlay: {
     flex: 1,
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    backgroundColor: 'transparent',
     justifyContent: 'center',
     alignItems: 'center',
   },
